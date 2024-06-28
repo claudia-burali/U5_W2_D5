3b5a60cd146d3f6cd0680c715dabcbfa70519916
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-06-28 14:46:44 +0200

    CRUD dipendente

--- src/main/java/claudiaburali/gestione_dispositivi/controllers/DipendenteController.java
+++ src/main/java/claudiaburali/gestione_dispositivi/controllers/DipendenteController.java
@@ -1,20 +1,27 @@
 package claudiaburali.gestione_dispositivi.controllers;
 
+import claudiaburali.gestione_dispositivi.entities.Dipendente;
 import claudiaburali.gestione_dispositivi.exceptions.BadRequestException;
 import claudiaburali.gestione_dispositivi.payloads.DipendenteDTO;
 import claudiaburali.gestione_dispositivi.payloads.DipententeResponseDTO;
 import claudiaburali.gestione_dispositivi.services.DipendenteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/dipendenti")
 public class DipendenteController {
     @Autowired
     private DipendenteService dipendenteService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public DipententeResponseDTO dipententeResponseDTO (@RequestBody @Validated DipendenteDTO dipendenteDTO, BindingResult bindingResult) {
@@ -22,5 +29,31 @@ public class DipendenteController {
             throw new BadRequestException(bindingResult.getAllErrors());
         }
         return new DipententeResponseDTO(dipendenteService.saveDipendente(dipendenteDTO).getId());
+    }
+
+    @GetMapping
+    public Page<Dipendente> getAllDipendenti(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size, @RequestParam(defaultValue = "id") String sortBy) {
+        return this.dipendenteService.getDipendenti(page, size, sortBy);
+    }
+
+    @GetMapping("/{dipendenteId}")
+    public Dipendente findById(@PathVariable UUID dipendenteId) {
+        return this.dipendenteService.findById(dipendenteId);
+    }
+
+    @PutMapping("/{dipendenteId}")
+    public Dipendente findByIdAndUpdate(@PathVariable UUID dipendenteId, @RequestBody Dipendente body) {
+        return this.dipendenteService.findByIdAndUpdate(dipendenteId, body);
+    }
+
+    @DeleteMapping("/{dipendenteId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void findByIdAndDelete(@PathVariable UUID dipendenteId) {
+        this.dipendenteService.findByIdAndDelete(dipendenteId);
+    }
+
+    @PostMapping("/{dipendenteId}/avatar")
+    public String uploadAvatar(@RequestParam("avatar") MultipartFile image) throws IOException {
+        return this.dipendenteService.uploadImage(image);
     }
 }
--- src/main/java/claudiaburali/gestione_dispositivi/services/DipendenteService.java
+++ src/main/java/claudiaburali/gestione_dispositivi/services/DipendenteService.java
@@ -2,11 +2,21 @@ package claudiaburali.gestione_dispositivi.services;
 
 import claudiaburali.gestione_dispositivi.entities.Dipendente;
 import claudiaburali.gestione_dispositivi.exceptions.BadRequestException;
+import claudiaburali.gestione_dispositivi.exceptions.NotFoundException;
 import claudiaburali.gestione_dispositivi.payloads.DipendenteDTO;
 import claudiaburali.gestione_dispositivi.repositories.DipendenteRepository;
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Service
 public class DipendenteService {
@@ -24,5 +34,34 @@ public class DipendenteService {
     Dipendente dipendenteForDb = new Dipendente(dipendenteDTO.username(), dipendenteDTO.name(), dipendenteDTO.surname(), dipendenteDTO.email());
     dipendenteForDb.setAvatarURL("https://png.pngtree.com/png-clipart/20191120/original/pngtree-outline-user-icon-png-image_5045523.jpg");
     return dipendenteRepository.save(dipendenteForDb);
+    }
+
+    public Page<Dipendente> getDipendenti(int page, int size, String sortBy) {
+        if (size > 50) size = 50;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return dipendenteRepository.findAll(pageable);
+    }
+
+    public Dipendente findById(UUID dipendenteId) {
+        return this.dipendenteRepository.findById(dipendenteId).orElseThrow(() -> new NotFoundException(dipendenteId));
+    }
+
+    public Dipendente findByIdAndUpdate(UUID dipendenteId, Dipendente modifiedDipendente) {
+        Dipendente found = this.findById(dipendenteId);
+        found.setUsername(modifiedDipendente.getUsername());
+        found.setName(modifiedDipendente.getName());
+        found.setSurname(modifiedDipendente.getSurname());
+        found.setEmail(modifiedDipendente.getEmail());
+        found.setAvatarURL("https://png.pngtree.com/png-clipart/20191120/original/pngtree-outline-user-icon-png-image_5045523.jpg");
+        return this.dipendenteRepository.save(found);
+    }
+
+    public void findByIdAndDelete(UUID dipendenteId) {
+        Dipendente found = this.findById(dipendenteId);
+        this.dipendenteRepository.delete(found);
+    }
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        return (String) cloudinaryUploader.uploader().upload(file.getBytes(), ObjectUtils.emptyMap()).get("url");
     }
 }
