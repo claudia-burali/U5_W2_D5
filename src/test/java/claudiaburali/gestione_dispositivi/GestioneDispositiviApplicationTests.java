283a41c4a4c48d753fb6e3bef7d2ab904f718e9e
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-06-28 10:43:39 +0200

    impostazione di base

---
+++ src/test/java/claudiaburali/gestione_dispositivi/GestioneDispositiviApplicationTests.java
@@ -0,0 +1,13 @@
+package claudiaburali.gestione_dispositivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneDispositiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
